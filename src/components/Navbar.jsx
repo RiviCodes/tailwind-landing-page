e1c4b7d91f9e975db0740bfaa5e866d1279c6ac8
RiviCodes <jose.riveramz@udlap.mx>
2022-06-22 14:01:57 -0500

    Add logo, navbar items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,33 @@
 import React from "react";
 
 export default function Navbar() {
-  return <></>;
+  return (
+    <nav className="relative container mx-auto p-6">
+      {/* Flex container */}
+      <div className="flex items-center justify-between">
+        {/* Logo */}
+        <div className="pt-2">
+          <img src="images/logo.svg" alt="" />
+        </div>
+        {/* Menu Items */}
+        <div className="hidden md:flex space-x-6">
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Product
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            About Us
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Careers
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue">
+            Community
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
