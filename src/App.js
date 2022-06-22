29bdf025ac2fd18583a0966f2235892a01695b68
RiviCodes <jose.riveramz@udlap.mx>
2022-06-22 15:09:37 -0500

    Import and render Features component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 }
