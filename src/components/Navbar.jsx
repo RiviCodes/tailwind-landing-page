75dd969c44f9b48c8fc8a350e7ee0ffb572d2a7f
RiviCodes <jose.riveramz@udlap.mx>
2022-06-23 14:08:00 -0500

    Implement burgen menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,6 +36,30 @@ export default function Navbar() {
         >
           Get Started
         </a>
+
+        {/* Hamburger icon */}
+        <button
+          id="menu-btn"
+          className="block hamburger md:hidden focus:outline-none"
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className="md:hidden">
+        <div
+          id="menu"
+          className="absolute flex flex-col items-center hidden self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md"
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
       </div>
     </nav>
   );
