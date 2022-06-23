8b75ec4bdce3f0c813c864795060a546d9471288
RiviCodes <jose.riveramz@udlap.mx>
2022-06-23 13:51:53 -0500

    Add input section, add button, style elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -64,6 +64,22 @@ export default function Footer() {
             </a>
           </div>
         </div>
+
+        {/* Input container */}
+        <div className="flex flex-col justify-between">
+          <form>
+            <div className="flex space-x-3">
+              <input
+                type="text"
+                className="flex-1 px-4 rounded-full focus:outline-none"
+                placeholder="Updates in your inbox"
+              />
+              <button className="px-6 py-2 text-white rounded-full bg-brightRed hover:bg-brightRedLight focus:outline-none">
+                Go
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </footer>
   );
