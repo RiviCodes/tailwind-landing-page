8302ac6ebfd06dca13a663d827963e10e9becd6f
RiviCodes <jose.riveramz@udlap.mx>
2022-06-23 13:22:54 -0500

    Add button

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -51,6 +51,15 @@ export default function Testimonials() {
             </p>
           </div>
         </div>
+
+        <div className="my-16">
+          <a
+            href="#"
+            className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight"
+          >
+            Get Started
+          </a>
+        </div>
       </div>
     </section>
   );
